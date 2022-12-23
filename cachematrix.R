a6e83c652d056bacaf41eb658fa842fe49d52b19
RyBres <bresnahanr18@gmail.com>
2022-12-23 16:58:28 -0500

    First submission of JH R Programming Cert's 'Programming Assignment 2: Lexical Scoping'.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Shown below are the code needed to create functions 'makeCacheMatrix' and 'cacheSolve', as well as some
+## code that tests the functions.
 
-## Write a short comment describing this function
+## 1. makeCacheMatrix creates the matrix object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        set <- function(y) {
+                x <- y
+                inv <- NULL
+        }
+        get <- function() x
+        setInv <- function(solve) inv <<- solve
+        getInv <- function() inv
+        list(set = set, get = get,
+             setInv = setInv,
+             getInv = getInv)
 }
 
-
-## Write a short comment describing this function
+## 2. cacheSolve computes the inverse of the matrix that was cached in makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getInv()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        data <- (x$get())
+        inv <- solve(data, ...)
+        x$setInv(inv)
+        inv
 }
+
+# 3. Testing makeCacheMatrix and cacheSolve on a 2 x 2 matrix
+m1 <- matrix(c(1/2, -1/4, -1, 3/4), nrow = 2, ncol = 2)
+myMatrix_object <- makeCacheMatrix(m1)
+cacheSolve(myMatrix_object)
